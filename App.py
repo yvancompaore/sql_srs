84e434d055669d742fa85a0ae4ce4037b0d0766b
yvancompaore <36414401+yvancompaore@users.noreply.github.com>
2024-02-09 22:09:49 +0100

    Premier commit initialisation

---
+++ App.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import duckdb
+
+st.write("MY STREAMLIT APPLICATION ")
+data = {"a": [1, 2, 3], "b": [3, 5, 6]}
+df = pd.DataFrame(data)
+
+tab1,tab2,tab3 = st.tabs(["Cat", "Dog", "Owl"])
+
+with tab1: 
+     input_text = st.text_area(label="Entrer votre valeur")
+     st.write(input_text)
+     st.write(df)
+     query = input_text
+     d = duckdb.query(query).df()
+     st.write(d)
+
+
+
